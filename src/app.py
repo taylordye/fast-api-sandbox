5170c64f29eea1d13dbef812b8993a7d638597fb
Taylor Dye <dyetaylor@icloud.com>
2021-01-28 23:17:36 -0500

    Fixed deployment errors

--- src/app.py
+++ src/app.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 import os
-from functools import lru_cache
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
 import pathlib
@@ -16,15 +15,9 @@ app = FastAPI()
 # Establish templates
 templates = Jinja2Templates(directory=BASE_DIR / "templates")
 
-# Cache the environment variable
-@lru_cache
-def cached_dotenv(): 
-    # Load the environment vars and set the environment variable path
-    dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
-    load_dotenv(dotenv_path=dotenv_path)
-
-# Get the .env
-cached_dotenv()
+# Load the environment vars and set the environment variable path
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path=dotenv_path)
 
 AIRTABLE_BASE_ID = os.getenv('AIRTABLE_BASE_ID')
 AIRTABLE_API_KEY = os.getenv('AIRTABLE_API_KEY')
